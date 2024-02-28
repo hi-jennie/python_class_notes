9bde65864f117fed3ce123150ae7fa5d85c09a75
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-02-28 20:31:10 +0800

    nested loops

---
+++ loop/mario.py
@@ -0,0 +1,12 @@
+# nested loops
+
+def main():
+    print_square(5)
+    
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#",end="")
+        print()
+        #or将前三排改为：print("#" * size)
+main()
