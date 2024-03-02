57bc24ee23a9e7e71b404502b82a187b202b68c7
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-03-02 17:33:06 +0800

    deep

---
+++ homework/set1/deep.py
@@ -0,0 +1,10 @@
+answer = input("What is the Answer to the Great Question of life, the Universe, and Everthing?").strip()
+
+answer = answer.upper()
+match answer:
+   case "42" :
+       print("Yes")
+   case "FORTY-TWO" | "FORTY TWO":
+       print("Yes")
+   case _:
+       print("No")
