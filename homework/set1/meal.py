7ca86605f00af8ddad6f81cfe776fc986289dc5f
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-03-04 19:26:28 +0800

    replace/split

---
+++ homework/set1/meal.py
@@ -0,0 +1,21 @@
+def main():
+    time = input("What time is it ?")
+    x = convert(time)
+    if 7 < x <= 8:
+        print("breakfast time")
+    elif 12 < x <= 13:
+        print("lunch time")
+    elif 18 < x <= 19:
+        print("dinner time")
+    else:
+        pass
+        
+
+
+def convert(m):
+    number = m.replace(":"," ").split()
+    n = int(number[0]) + int(number[1]) / 60
+    return n
+
+if __name__ == "__main__":
+    main()
