b3493261d352ff3669ef4ddbc5a69af5f447c060
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-03-05 17:44:26 +0800

    while True

---
+++ homework/set2/coke.py
@@ -0,0 +1,23 @@
+
+print("Amount Due : 50")
+a = 50
+while True:
+  i = int(input("Insert Coin: "))
+  if i in [50, 25, 10 ,5]:
+        a = a - i
+        if a > 0:
+          print(f"Amount Due: {a}")
+        elif a == 0 :
+           print("Change Owed: 0")
+           break
+        elif a < 0 :
+           print(f"Change Owed: {-a}")
+           break
+  elif i not in [50, 25, 10, 5]:
+      print(f"Amount Due: {a}")
+
+
+      
+  
+    
+    
