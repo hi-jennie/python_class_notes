9ffa6eb14bb10e50e3b91c35e00325128dc6728c
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-04-16 20:54:27 +0800

    set/map

--- lesson8_OOP/decorator.py
+++ lesson8_OOP/decorator.py
@@ -1,22 +1,12 @@
-from collections import Counter
-
-shoes_number = int(input())
-shoes_size = input().split()
-purchase_number = int(input().strip())
-inventory = dict(Counter(shoes_size))
-
-total_money = []
-
-def uesful_number(purchase_number):
-    while purchase_number > 0:
-        size, money = input().split()
-        if size in inventory and int(inventory[size]) > 0:
-            total_money.append(int(money))
-            inventory[size] = int(inventory[size]) - 1
-        else:
-            pass
-        purchase_number -=1 
+def average(array):
+    # your code goes here
+    arr = set(array)
+    result = sum(arr) / len(arr)
+    return f"{result:.3f}"
     
-uesful_number(purchase_number)
-
-print(sum(total_money))
+    
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
