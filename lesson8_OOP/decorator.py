924a7a7925ee674cfea128405bdb1fc8c8b62f20
JENNIE <160710985+hi-jennie@users.noreply.github.com>
2024-04-16 21:27:34 +0800

    set()

--- lesson8_OOP/decorator.py
+++ lesson8_OOP/decorator.py
@@ -1,12 +1,1 @@
-def average(array):
-    # your code goes here
-    arr = set(array)
-    result = sum(arr) / len(arr)
-    return f"{result:.3f}"
-    
-    
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    result = average(arr)
-    print(result)
+
